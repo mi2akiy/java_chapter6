f549019766aca0ccf46a0ff701ba4131b7d8e351
User <User@192.168.11.12>
2023-12-07 14:05:13 +0900

    [finish]6-2

---
+++ src/java_chapter6/Student.java
@@ -0,0 +1,14 @@
+package java_chapter6;
+
+public class Student {
+	private String name;
+
+	public Student(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return "私の名前は" + name + "です";
+	}
+	
+	
+}
