98e647d053284846ce4acc620de27b9e8840e04a
mi2aki <mi2akiy@gmail.com>
2023-12-07 16:28:58 +0900

    [finish]6-6

--- src/java_chapter6/ArrayListSample.java
+++ src/java_chapter6/ArrayListSample.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 public class ArrayListSample {
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
+		System.out.println(list.size());
 		list.add("Java入門講座");
 		list.add("ここまで");
 		list.add("よく頑張りましたね！");
+		System.out.println(list.size());
+		
+		list.remove(1);
+		System.out.println(list.size());
 
 		for (int i = 0; i < list.size(); i++) {
 			System.out.print(list.get(i));
